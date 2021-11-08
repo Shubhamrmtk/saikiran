648f14d8cad0af7d8384d505bf87cd6ffb797065
Shubhamrmtk <shubhamramteke04@gmail.com>
2021-11-08 12:50:18 +0530

    anyhting

---
+++ 10th_sep.py
@@ -0,0 +1,39 @@
+# import requests
+# from bs4 import BeautifulSoup
+# with open('file:///home/shubham/fridaybeautifulsoup.html','r')
+# def table(a,b):
+#     if a==1 :
+#         return 1
+#     return b*table(a-1,b)
+# print(table(10,5))
+    
+# def pow(n) :
+#     if n==1 :
+#         return 2**n
+#     return 2*pow(n-1)
+# # print(pow(3))
+# def n(a) :
+#     if a==0 :
+#         return 0
+#     # print(a)
+#     n(a-1)
+#     print((a*5))
+# print(n(10))
+
+# print(100*("string\n\n"))
+# print()
+d=""
+# for i in (a.split(",")
+# for i in a :
+#     if i.isalpha() :
+#         d=d+i
+# print(d)
+# for i in (a.split(",")) :
+#     d=d+i
+# print(type(d))
+a=['saikira','bhopland','petland','bhatland']
+f=[]
+for i in a[::-1] :
+    f.append(i[::-1])
+print(f)
+
